3db37738123ea8723d59b4614061c8a810ddf085
Naveen <naveenkrishnawodeyar@gmail.com>
2024-09-06 23:32:24 +0530

    springBatch related project, initial commit

---
+++ MultiFile_Processing/src/main/java/com/batch/MultiFile_Processing/MultiFileProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.batch.MultiFile_Processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFileProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiFileProcessingApplication.class, args);
+	}
+
+}
---
+++ MultiFile_Processing/src/test/java/com/batch/MultiFile_Processing/MultiFileProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.MultiFile_Processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiFileProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
