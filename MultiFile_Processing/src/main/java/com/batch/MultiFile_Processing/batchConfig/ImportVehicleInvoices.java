5562d7f923c311e517cddf1afa0ff8b3a92dec87
Naveen <naveenkrishnawodeyar@gmail.com>
2024-09-08 20:11:37 +0530

    schedulers need to be added

--- MultiFile_Processing/src/main/java/com/batch/MultiFile_Processing/batchConfig/ImportVehicleInvoices.java
+++ MultiFile_Processing/src/main/java/com/batch/MultiFile_Processing/batchConfig/ImportVehicleInvoices.java
@@ -2,11 +2,15 @@ package com.batch.MultiFile_Processing.batchConfig;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -21,12 +25,20 @@ import com.batch.MultiFile_Processing.entity.VehicleDTO;
 public class ImportVehicleInvoices {
 	
 	private static final Logger log = LoggerFactory.getLogger(ImportVehicleInvoices.class);
+	
+	@Bean
+	public Job importVehicelJob(JobRepository jobRepository, Step importVehiclesStep) {
+		return new JobBuilder("importVehicleJob",jobRepository)
+				.incrementer(new RunIdIncrementer())
+				.start(importVehiclesStep)
+				.build();
+	}
 
 	public Step importVehiclesStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
 		return new StepBuilder("importVehicleStep",jobRepository)
 				.chunk(100,platformTransactionManager)
 				.reader(vehicleFileItemReader())
-				.processor(item->vehicleProcessor(item))
+				.processor(item->vehicleProcessor((VehicleDTO) item))
 				.writer(items->log.info("Writing items: {}",items))
 				.build();
 	}
