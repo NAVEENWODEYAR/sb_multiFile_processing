caa78a392912a7dd34bd89e05e2a74b733fddd33
Naveen <naveenkrishnawodeyar@gmail.com>
2024-09-11 20:56:58 +0530

    initially applicaion is UP&running

--- Employee_Batch_Processing_Demo/src/main/java/com/example/demo/EmployeeBatchProcessingDemoApplication.java
+++ Employee_Batch_Processing_Demo/src/main/java/com/example/demo/EmployeeBatchProcessingDemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EmployeeBatchProcessingDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeBatchProcessingDemoApplication.class, args);
 	}
+	
+	 @Bean
+	    public CommandLineRunner printJavaVersion() {
+	        return args -> {
+	            String javaVersion = System.getProperty("java.version");
+	            System.out.println("\n#############################\n");
+	            System.out.println("JAVA VERSOIN: " + javaVersion);
+	            System.out.println("\n#############################\n");
+	        };
+	    }
 
 }
