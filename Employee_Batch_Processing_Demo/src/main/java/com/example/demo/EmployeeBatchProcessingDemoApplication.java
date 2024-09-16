528f0abc553d2eb53c97acf93aa83a13d4c0adc2
Naveen <naveenkrishnawodeyar@gmail.com>
2024-09-16 22:36:15 +0530

    job config error

--- Employee_Batch_Processing_Demo/src/main/java/com/example/demo/EmployeeBatchProcessingDemoApplication.java
+++ Employee_Batch_Processing_Demo/src/main/java/com/example/demo/EmployeeBatchProcessingDemoApplication.java
@@ -3,9 +3,15 @@ package com.example.demo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableRetry
+@EnableCaching
+@EnableJpaRepositories
 @SpringBootApplication
 @EnableScheduling
 public class EmployeeBatchProcessingDemoApplication {
@@ -14,14 +20,14 @@ public class EmployeeBatchProcessingDemoApplication {
 		SpringApplication.run(EmployeeBatchProcessingDemoApplication.class, args);
 	}
 	
-	 @Bean
-	    public CommandLineRunner printJavaVersion() {
-	        return args -> {
-	            String javaVersion = System.getProperty("java.version");
-	            System.out.println("\n#############################\n");
-	            System.out.println("JAVA VERSOIN: " + javaVersion);
-	            System.out.println("\n#############################\n");
-	        };
-	    }
+	@Bean
+    public CommandLineRunner printJavaVersion() {
+        return args -> {
+            String javaVersion = System.getProperty("java.version");
+            System.out.println("\n#############################\n");
+            System.out.println("JAVA VERSION: " + javaVersion);
+            System.out.println("\n#############################\n");
+        };
+    }
 
 }
